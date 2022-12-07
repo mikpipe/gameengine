13857831d1b1d559e7da3bcae5921f14bade59bc
26918@students.hsrw <jonas-josef.holm@hsrw.org>
2022-12-07 23:55:32 +0100

    VAOs & VBOs triangle rendering

--- src/PackageEngineTester/MainGameLoop.java
+++ src/PackageEngineTester/MainGameLoop.java
@@ -2,17 +2,38 @@ package PackageEngineTester;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
     public static void main(String[] agrs){
         DisplayManager.createDisplay();
 
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        float[] vertices = {
+                -0.5f, 0.5f, 0,
+                -0.5f, -0.5f, 0,
+                0.5f, -0.5f, 0,
+                0.5f, -0.5f, 0,
+                0.5f, -0.5f, 0,
+                0.5f, 0.5f, 0,
+                -0.5f, 0.5f, 0,
+
+        };
+
+        RawModel model = loader.loadToVAO(vertices);
+
         while (!Display.isCloseRequested()){
-            // Game logic
-            // Rendering
+            renderer.prepare();
+
+            renderer.render(model);
             DisplayManager.updateDisplay();
         }
 
+        loader.CleanUp();
         DisplayManager.closeDisplay();
     }
 }
